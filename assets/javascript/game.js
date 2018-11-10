804cfa67d1794bf7b4bc9492184d2bb593c09e2d
bareseph <barryriveroll@gmail.com>
2018-11-09 23:28:13 -0500

    Arrange divs to be more intuitive, bug fixes, change some art

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var goblins = {
     goblinArray: [
         goblin1 = {
-            name: "Goblin Name 1",
+            name: "Pirate Goblin",
             nameElement: $("#goblin-1-name"),
             maxHP: 150,
             currentHP: 110,
@@ -12,7 +12,7 @@ var goblins = {
             card: $("#goblin-1")
         },
         goblin2 = {
-            name: "Goblin 2",
+            name: "Fishing Goblin",
             nameElement: $("#goblin-2-name"),
             maxHP: 120,
             currentHP: 90,
@@ -23,7 +23,7 @@ var goblins = {
             card: $("#goblin-2")
         },
         goblin3 = {
-            name: "Goblin 3",
+            name: "Potion Goblin",
             nameElement: $("#goblin-3-name"),
             maxHP: 180,
             currentHP: 80,
@@ -34,7 +34,7 @@ var goblins = {
             card: $("#goblin-3")
         },
         goblin4 = {
-            name: "Goblin 4",
+            name: "Mobster Goblin",
             nameElement: $("#goblin-4-name"),
             maxHP: 100,
             currentHP: 100,
@@ -54,20 +54,29 @@ var game = {
     heroSelect: false,
     enemySelect: false,
     combatText: $("#combat-text"),
-    gobName1: $("#goblin-1-name").hide(),
-    gobName2: $("#goblin-2-name").hide(),
-    gobName3: $("#goblin-3-name").hide(),
-    gobName4: $("#goblin-4-name").hide(),
+    // gobName1: $("#goblin-1-name").hide(),
+    // gobName2: $("#goblin-2-name").hide(),
+    // gobName3: $("#goblin-3-name").hide(),
+    // gobName4: $("#goblin-4-name").hide(),
+    goalText: $("#goal-text"),
     
     newGame: function() {
         for (var i = 0; i < goblins.goblinArray.length; i++) {
+            // goblins.goblinArray[i].nameElement.hide();
             goblins.goblinArray[i].currentAttack = goblins.goblinArray[i].baseAttack;
             goblins.goblinArray[i].currentHP = goblins.goblinArray[i].maxHP;
             goblins.goblinArray[i].healthBar.css("width", 190);
             goblins.goblinArray[i].healthText.text(goblins.goblinArray[i].currentHP + "/" + goblins.goblinArray[i].maxHP);
             goblins.goblinArray[i].nameElement.text(goblins.goblinArray[i].name);
+            goblins.goblinArray[i].nameElement.arctext({radius: 350});
         }
         $("#restart-button").hide();
+        
+        // game.gobName2.show().arctext({radius: 350});
+        // game.gobName3.show().arctext({radius: 350});
+        // game.gobName4.show().arctext({radius: 350});
+        game.goalText.text("Choose your goblin!");
+        $("#enemy-select").css("width", "960px");
     },
 
     updateGameText: function() {
@@ -82,18 +91,24 @@ var game = {
     },
 
     attackButton: function() {
-        goblins.currentHero.card.animate({ left: "+=400px" }, 60);
+        goblins.currentHero.card.animate({ left: "+=550px" }, 60);
+        $("#attack-button").attr("id", "disable-button");
         setTimeout (function() {
-            goblins.currentHero.card.animate({ left: "-=400px" }, 450);
+            goblins.currentHero.card.animate({ left: "-=550px" }, 450);
         }, 60);
 
         setTimeout (function() {
-            goblins.currentEnemy.card.animate({ left: "-=400px" }, 60);
+            goblins.currentEnemy.card.animate({ left: "-=550px" }, 60);
 
             setTimeout (function() {
-                goblins.currentEnemy.card.animate({ left: "+=400px" }, 450);
+                goblins.currentEnemy.card.animate({ left: "+=550px" }, 450);
             }, 60);
         }, 400);
+
+        setTimeout (function() {
+            $("#disable-button").attr("id", "attack-button");
+        }, 700);
+        
         
         goblins.currentEnemy.currentHP -= goblins.currentHero.currentAttack;
         this.changeCombatText("<div>You attacked <span id='combat-enemy-name'>" + goblins.currentEnemy.name + " </span> for " + goblins.currentHero.currentAttack + " damage!");
@@ -101,6 +116,12 @@ var game = {
             this.changeCombatText("<div>You killed <span id='combat-enemy-name'>" + goblins.currentEnemy.name + "</span>!");
             this.enemySelect = false;
             $("#current-enemy").children().removeClass("choose-enemy").hide();
+            if (goblins.goblinArray.length === 0) {
+                game.goalText.text("You won!!!");
+                $("#restart-button").show();
+            } else {
+                game.goalText.text("Choose your next enemy...");
+            }
         } else {
             goblins.currentHero.currentHP -= goblins.currentEnemy.baseAttack;
             setTimeout (function() {
@@ -147,6 +168,7 @@ $(document).ready(function() {
         
         if (!game.enemySelect) {
             if (classString.includes('choose-hero')) {
+                $("#enemy-select").css("width", "-=240px");
                 goblins.goblinArrayClone = goblins.goblinArray.slice();
                 var gobIndex = $(".choose-hero").index(this);
                 $("#enemy-select").append($(".goblin-card"));
@@ -155,20 +177,25 @@ $(document).ready(function() {
                 $(this).removeClass("choose-enemy");
                 goblins.currentHero = goblins.goblinArray[gobIndex];
                 game.heroSelect = true;
+                goblins.goblinArray.splice(gobIndex, 1);
+                game.goalText.text("Choose your enemy...");
             } else if (classString.includes('choose-enemy')) {
+                $("#enemy-select").css("width", "-=240px");
                 var gobIndex = $(".choose-enemy").index(this);
                 $("#current-enemy").append($(this));
                 goblins.currentEnemy = goblins.goblinArray[gobIndex];
                 game.enemySelect = true;
+                goblins.goblinArray.splice(gobIndex, 1);
+                game.goalText.text("Fight!!");
             }
 
-            goblins.goblinArray.splice(gobIndex, 1);
+            
         }
     });
 
     $("button").on("click", function(e) {
-        var classString = e.currentTarget.getAttribute('id');
-        if (classString.includes('attack-button')) {
+        var idString = e.currentTarget.getAttribute('id');
+        if (idString.includes('attack-button')) {
             if (goblins.currentHero.currentHP <=0) {
                 game.changeCombatText("<div>You died! Click Restart!");
             } else if (game.enemySelect == true) {
@@ -176,14 +203,13 @@ $(document).ready(function() {
             } else {
                 game.changeCombatText("<div>There is nothing to attack!");
             }
+        } else if (idString.includes('disable-button')) {
+            game.changeCombatText("<div>Attacking...");
         } else {
             game.restartGame();
         }
     }),
     
     game.newGame();
-    game.gobName1.show().arctext({radius: 500});
-    game.gobName2.show().arctext({radius: 500});
-    game.gobName3.show().arctext({radius: 500});
-    game.gobName4.show().arctext({radius: 500});
+    
 });
